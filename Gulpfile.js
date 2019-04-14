067267ac35e964b42ca1ec93b3a4fb9ca5e315e2
acj <gitlab@acjs.net>
2019-04-14 20:04:45 +0200

    Gulp Sass.

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+var gulp = require('gulp');
+var pipeline = require('readable-stream').pipeline;
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('./css'))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
