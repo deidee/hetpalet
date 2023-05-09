61dad33db0d3a35e7f7396e9217d76c3884022f5
ACJ <gitlab@acjs.net>
2023-05-09 15:01:14 +0200

    Parse _deJade()_

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
 
-module.exports = config => {
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addNunjucksGlobal("deJade", function() {
+        let r = Math.round(127 * Math.random());
+        let g = Math.round(Math.random() * 128 + 127);
+        let b = Math.round(191 * Math.random());
+
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    });
+
     return {
         dir: {
             input: 'src',
             output: 'dist'
-        }
+        },
+        markdownTemplateEngine: "njk"
     };
 };
